75227f13904426af929e5a1ce690808b777e1fda
wkwkland69 <51428441+wkwkland69@users.noreply.github.com>
2022-11-06 19:02:01 +0700

    Add files via upload

---
+++ Black jack/Blackjack-master/src/elements/Card.java
@@ -0,0 +1,32 @@
+package elements;
+/* elements.Card object
+ Author: Vaidas Vaitkevicius
+ Date: 11/10/2018
+ */
+
+public class Card {
+
+    private String identifier;
+    private String suit;
+
+
+    /**
+     * elements.Card constructor
+     *
+     * @param name name of a card (e.g. 2h which evaluates to: two of hearts)
+     */
+    public Card (String name) {
+        String[] parts = name.split("");
+        this.identifier = parts[0];
+        this.suit = parts[1];
+    }
+
+    public String getId() {
+        return this.identifier;
+    }
+
+    public String getSuit() {
+        return this.suit;
+    }
+
+}
